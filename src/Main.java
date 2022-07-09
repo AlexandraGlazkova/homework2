3ef594756f3dcd36f67c472201a24bdc8bffbca6
GlakovaA <22109@bk.ru>
2022-07-09 22:54:57 +0300

    Задача  номер 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int number = 2;
+        System.out.println(number);
+        byte g = 12;
+        System.out.println(g);
+        short x = 10000;
+        System.out.println(x);
+        long y =-20;
+        System.out.println(y);
+        float a = 4.88888888f;
+        System.out.println(a);
+        float b = (float) 4.88888888;
+        System.out.println(b);
+        double t = 4.88888888;
+        System.out.println(t);
+
     }
 }
